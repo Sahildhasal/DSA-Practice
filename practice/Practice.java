a2ca09299cc776a307da6835d6de253309ba81e4
Sahildhasal <sahildhasal.sd@gmail.com>
2025-10-10 06:10:02 +0530

    Under stand binary search approach for sorted arrays/rotated sorted array

--- practice/Practice.java
+++ practice/Practice.java
@@ -3,24 +3,36 @@ package practice;
 public class Practice {
 
     public static void main(String[] args) {
-        // Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+        // Input: nums = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
         // Output: 6
         // Explanation: [4, -1, 2, 1] has the largest sum = 6.
-        int[] numsArray = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] numsArray = {4,5,6,7,0,1,2};
 
         int result = findBestResult(numsArray);
         System.out.println("result is " + result);
     }
 
     public static int findBestResult(int[] numsArray) { 
-        int globalMaxSum = numsArray[0];
-        int currentMaxSum = numsArray[0];
 
-        for (int i = 0; i < numsArray.length; i++) {
-            currentMaxSum = Math.max(numsArray[i], numsArray[i] + currentMaxSum);
+        int left = 0;
+        int right = numsArray.length - 1;
 
-            globalMaxSum = Math.max(currentMaxSum, globalMaxSum);
+        
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            System.out.println("left: " + left + " right: " + right + " mid:" + mid );
+            if(numsArray[mid] > numsArray[right]){
+                left = mid + 1;
+                System.out.println("enter if");
+            }
+            else {
+                right = mid;
+                System.out.println("enter else");
+            }
         }
-       return globalMaxSum;
+
+
+        return numsArray[left];
     }
 }
