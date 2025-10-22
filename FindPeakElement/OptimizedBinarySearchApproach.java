bc238f7094e91bafb5121a380fc37f717b1bbf45
Sahildhasal <sahildhasal.sd@gmail.com>
2025-10-22 17:34:43 +0530

    Find a peak element

---
+++ FindPeakElement/OptimizedBinarySearchApproach.java
@@ -0,0 +1,33 @@
+package FindPeakElement;
+
+public class BruteForceApproach {
+    public static void main(String[] args) {
+        int[] numsArray = {1,2,3,99,1};
+        int target = 7;    
+        
+        int result = findBestResult(numsArray, target);
+
+        System.out.println("result is " + result);
+    }   
+
+
+    public static int findBestResult(int[] numsArray, int target){
+
+        int left = 0;
+        int right = numsArray.length - 1;
+
+
+        while(left < right) {
+            int mid = left + (right - left)/2;
+            
+            if(numsArray[mid] > numsArray[mid + 1]){
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }   
+}
